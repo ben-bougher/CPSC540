c31ad5c493bc0e8722067dbf008174c1428ab756
Ben Bougher <ben.bougher@gmail.com>
2014-12-03 14:09:45 -0800

    some comments

--- coding_project/regression_NW_demo.m
+++ coding_project/regression_NW_demo.m
@@ -2,20 +2,26 @@
 load data_regressOnOne.mat
 
 
-h = .1; % Amount of smoothing
+%% Set the amount of smoothing
+h = .1;
 kernel_options = [];
 kernel_options.h = h;
 
-[gauss_kern] = matLearn_kernel_Gaussian(kernel_options);
-[laplace_kern] = matLearn_kernel_Laplace(kernel_options);
+%% Generate the kernels
+[kern] = matLearn_kernel_Gaussian(kernel_options);
+%[kern] = matLearn_kernel_Laplace(kernel_options);
 
+
+%% Generate the NW regression object
 reg_options = [];
-reg_options.kernel = gauss_kern;
+reg_options.kernel = kern;
 [model] = matLearn_regression_NW(Xtrain, ytrain, reg_options);
 
-
+%% Make predictions on test data
 yhat = model.predict(model, Xtest);
 
+
+%% Plot the results
 figure()
 scatter(Xtest, ytest);
 figure()
